9b545c23bb319e285942f72af1e9b9d64e53d51d
Doston Nabotov <dostonnabotov@gmail.com>
2022-06-29 23:01:53 +0300

    bug fixes

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 - Add "increment" buttons by 1, 2, 3
 - "New Game" button
 - Highlight the leader
-- Add a few more counter (e.g. period, fouls, timer)
+- Add a few more counter (e.g. period, fouls, timer) (in progress...)
--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 const homeScore = document.getElementById('home-score');
 const guestScore = document.getElementById('guest-score');
 const compareScore = document.getElementById('compare');
-const time = document.getElementById('time');
-
-let countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-console.log(countDownDate);
-
-let x =  setInterval(function() {
-    // Get today's date and time
-  let now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  let distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  time.innerHTML = `${hours} : ${minutes} : ${seconds}`;
-
-  if (distance < 0) {
-    clearInterval(x);
-    time = "EXPIRED";
-  }
-}, 1000);
-
 
 
 let HOME_SCORE = 0;
